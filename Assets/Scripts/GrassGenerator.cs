7db7bf9bc351a83edc257fc585f51556e5a56b2e
ElizabethBallou <epb267@nyu.edu>
2018-11-27 09:40:08 -0500

    added "press 'p' to poop"

---
+++ Assets/Scripts/GrassGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassGenerator : MonoBehaviour {
+	private ParticleSystem grass;
+	private Rigidbody rb;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		rb = this.GetComponent<Rigidbody>();
+		grass = this.transform.GetComponentInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (rb.velocity.magnitude > 0.5)
+		{
+			grass.Emit(1);
+		}
+	}
+
+
+	
+}
