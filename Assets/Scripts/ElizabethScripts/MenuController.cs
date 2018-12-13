18a7b6530395fd0e0f2b5803d24cbd3aaa44acf2
ElizabethBallou <epb267@nyu.edu>
2018-12-12 21:19:45 -0500

    Added working menu

---
+++ Assets/Scripts/ElizabethScripts/MenuController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+	private bool MenuIsActive;
+	public GameObject RestartLevel;
+	public GameObject PoopParty;
+	public GameObject Tutorial;
+	public GameObject Quit;
+	
+
+	// Use this for initialization
+	void Start () {
+		RestartLevel.SetActive(false);
+		PoopParty.SetActive(false);
+		Tutorial.SetActive(false);
+		Quit.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			
+			
+			
+		}
+
+		
+	}
+
+	public void ShowMenu()
+	{
+		if (MenuIsActive)
+		{
+			MenuIsActive = false;
+			RestartLevel.SetActive(false);
+			PoopParty.SetActive(false);
+			Tutorial.SetActive(false);
+			Quit.SetActive(false);
+		}
+		else if (!MenuIsActive)
+		{
+			MenuIsActive = true;
+			RestartLevel.SetActive(true);
+			PoopParty.SetActive(true);
+			Tutorial.SetActive(true);
+			Quit.SetActive(true);
+		}
+	}
+	
+}
